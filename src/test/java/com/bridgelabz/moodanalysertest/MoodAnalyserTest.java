03d5ba0aece9b857f712b6d66c0a2f73655de95f
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-05-02 21:25:22 +0530

    Added: Refactor the code to take the mood message in Constructor

--- src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalysertest/MoodAnalyserTest.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.moodanalysertest;
 
 import com.bridgelabz.moodanalyser.MoodAnalyser;
+import com.bridgelabz.moodanalyser.RefactorMoodAnalyser;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,6 +29,26 @@ public class MoodAnalyserTest {
     @Test
     public void givenMessage_IAmInSadMood_ShouldReturnSad(){
         String mood = moodAnalyser.analyseMood("I am in sad mood");
+        Assert.assertEquals("sad", mood);
+    }
+
+    /*
+     *When given happy message then it should return happy
+     */
+    @Test
+    public void givenMessage_InHappyMood_ShouldReturnHappy(){
+        RefactorMoodAnalyser moodAnalyser1 = new RefactorMoodAnalyser("I am in happy mood");
+        String mood = moodAnalyser1.analyseMood();
+        Assert.assertEquals("happy", mood);
+    }
+
+    /*
+     *When given sad message then it should return sad
+     */
+    @Test
+    public void givenMessage_SadMood_ShouldReturnHappy(){
+        RefactorMoodAnalyser moodAnalyser1 = new RefactorMoodAnalyser("I am in sad mood");
+        String mood = moodAnalyser1.analyseMood();
         Assert.assertEquals("sad", mood);
     }
 }
