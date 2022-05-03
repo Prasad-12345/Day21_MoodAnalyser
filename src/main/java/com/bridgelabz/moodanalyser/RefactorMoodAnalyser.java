284e6397eeeff224d8881240da6f37d33be4006a
Prasad-12345 <prasadsomvanshi471@gmail.com>
2022-05-03 09:21:11 +0530

    Added: Custom exception to handle null and empty message

--- src/main/java/com/bridgelabz/moodanalyser/RefactorMoodAnalyser.java
+++ src/main/java/com/bridgelabz/moodanalyser/RefactorMoodAnalyser.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.moodanalyser;
 /*
  *Author: Prasad
- *Handle Exception if User Provides Invalid Mood
+ *Handle Exception if User Provides Invalid Moodgi
  */
 public class RefactorMoodAnalyser {
     String message;
@@ -19,16 +19,23 @@ public class RefactorMoodAnalyser {
     /*
      *This method will return happy or sad also handle null pointer exception
      */
-    public String analyseMood(){
+    public String analyseMood() throws MoodAnalyzerException{
         try {
+     //       if(message.length() == 0){
+       //         throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.ENTERED_EMPTY, "Enter Proper Message. EMPTY Not Allowed");
+         //   }
             if (message.toLowerCase().contains("sad")) {
                 return "sad";
-            } else if(message.toLowerCase().contains("happy")){
+            }
+            else if(message.toLowerCase().contains("happy")){
                 return "happy";
             }
         }
         catch(NullPointerException e){
-            return  "happy";
+            throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.ENTERED_NULL, "Enter Proper Message. NULL Not Allowed");
+        }
+        catch (MoodAnalyzerException e){
+            throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.ENTERED_EMPTY, "Enter proper message. Empty not allowed");
         }
         return message;
     }
